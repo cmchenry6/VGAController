19b1cb03271ee8360437db8fa230140f16488099
cmchenry6 <cmchenry66@gmail.com>
2023-10-15 23:00:25 -0400

    added collisions with the top of the block

--- top_breakout.v
+++ top_breakout.v
@@ -48,8 +48,8 @@ localparam PDL_HEIGHT = 10;
 assign pdl_r = pdl_l + PDL_SIZE;
 
 // blocks properties
-localparam BLOCK_SIZE = 80;
-localparam BLOCK_HEIGHT = 20;
+localparam BLOCK_SIZE = 90;
+localparam BLOCK_HEIGHT = 15;
 typedef struct packed {
 	reg [9:0] bx, by;
 	reg [11:0] color;
@@ -58,12 +58,14 @@ typedef struct packed {
 //Top row
 wire row1en;
 block row1 [4:0];
+localparam ROW1_COLOR = 12'hFF0; // yellow
+localparam ROW1_Y = 60;
 initial begin
-row1[0].bx = 60; row1[0].by = 30; row1[0].color = 12'hFF0;
-row1[1].bx = row1[0].bx + BLOCK_SIZE + 30; row1[1].by = 30; row1[1].color = 12'hFF0;
-row1[2].bx = row1[1].bx + BLOCK_SIZE + 30; row1[2].by = 30; row1[2].color = 12'hFF0;
-row1[3].bx = row1[2].bx + BLOCK_SIZE + 30; row1[3].by = 30; row1[3].color = 12'hFF0;
-row1[4].bx = row1[3].bx + BLOCK_SIZE + 30; row1[4].by = 30; row1[4].color = 12'hFF0;
+row1[0].bx = 30; row1[0].by = ROW1_Y; row1[0].color = ROW1_COLOR;
+row1[1].bx = row1[0].bx + BLOCK_SIZE + 30; row1[1].by = ROW1_Y; row1[1].color = ROW1_COLOR;
+row1[2].bx = row1[1].bx + BLOCK_SIZE + 30; row1[2].by = ROW1_Y; row1[2].color = ROW1_COLOR;
+row1[3].bx = row1[2].bx + BLOCK_SIZE + 30; row1[3].by = ROW1_Y; row1[3].color = ROW1_COLOR;
+row1[4].bx = row1[3].bx + BLOCK_SIZE + 30; row1[4].by = ROW1_Y; row1[4].color = ROW1_COLOR;
 end
 
 // states
@@ -111,6 +113,18 @@ always@(posedge pix_clk) begin
 
 	if (frame && (state == PLAY)) begin
 		if (qdy == 0) begin // evaluate vertical(moving down)
+			integer k;
+			for (k = 0; k < 5; k = k + 1) begin
+				if (~row1[k].aliven) begin
+					if ((qy >= ROW1_Y - Q_SIZE) && (qy < ROW1_Y + BLOCK_HEIGHT) 
+					     && (qx >= row1[k].bx) && (qx < row1[k].bx + BLOCK_SIZE)) begin
+						qy <= 0; 
+						qdy <= 1;
+						row1[k].aliven <= 1;
+					end
+				end
+			end
+
 			if ((qy + Q_SIZE + qs >= V_RES - PDL_HEIGHT - 1) // colliding with paddle? 
 			      && (qx >= pdl_l && qx <= pdl_r)) begin
 				qy <= V_RES - PDL_HEIGHT - Q_SIZE;
@@ -120,6 +134,7 @@ always@(posedge pix_clk) begin
 				end else begin
 					qdx <= (qx >= (pdl_l + PDL_SIZE/2) && qx <= pdl_r) ? 0 : 1;
 				end
+
 			end else if (qy + Q_SIZE + qs >= V_RES - 1) begin // hitting bottom of screen?
 				state = RESET;
 			end else 
@@ -128,8 +143,9 @@ always@(posedge pix_clk) begin
 			integer j;
 			for (j = 0; j < 5; j = j + 1) begin
 				if (~row1[j].aliven) begin
-					if ((qy <= row1[j].by + BLOCK_HEIGHT) && (qy > row1[j].by) 
+					if ((qy <= ROW1_Y + BLOCK_HEIGHT) && (qy > ROW1_Y) 
 					     && (qx >= row1[j].bx) && (qx < row1[j].bx + BLOCK_SIZE)) begin
+						qy <= row1[j].by + BLOCK_HEIGHT;
 						qdy <= 0;
 						row1[j].aliven <= 1;
 					end
@@ -165,7 +181,7 @@ end
 assign q = (x_pix >= qx) && (x_pix < qx + Q_SIZE) && (y_pix >= qy) && (y_pix < qy + Q_SIZE);
 assign pdl = (x_pix >= pdl_l) && (x_pix < pdl_r) && (y_pix >= V_RES - PDL_HEIGHT);
 // assign row1en = (((x_pix >= row1[0].bx) && (x_pix < row1[0].bx + BLOCK_SIZE)) || ((x_pix >= row1[1].bx) && (x_pix < row1[1].bx + BLOCK_SIZE)) || ((x_pix >= row1[2].bx) && (x_pix < row1[2].bx + BLOCK_SIZE)) || ((x_pix >= row1[3].bx) && (x_pix < row1[3].bx + BLOCK_SIZE)) || ((x_pix >= row1[4].bx) && (x_pix < row1[4].bx + BLOCK_SIZE))) && (y_pix >= row1[0].by) && (y_pix < row1[0].by + BLOCK_HEIGHT);
-assign row1en = ((x_pix >= row1[0].bx) && (x_pix < row1[4].bx + BLOCK_SIZE) && (y_pix >= row1[0].by) && (y_pix < row1[4].by + BLOCK_HEIGHT));
+assign row1en = ((x_pix >= row1[0].bx) && (x_pix < row1[4].bx + BLOCK_SIZE) && (y_pix >= ROW1_Y) && (y_pix < ROW1_Y + BLOCK_HEIGHT));
 
 reg[3:0] paint_r;
 reg[3:0] paint_g;
@@ -180,7 +196,7 @@ always@(*) begin
 		integer i;
 		for (i = 0; i < 5; i = i + 1) begin
 			if ((x_pix >= row1[i].bx) && (x_pix < (row1[i].bx + BLOCK_SIZE))) begin
-				{paint_r, paint_g, paint_b} = (row1[i].aliven == 0) ? row1[i].color : 12'h137;
+				{paint_r, paint_g, paint_b} = (row1[i].aliven == 0) ? ROW1_COLOR : 12'h137;
 				break;
 			end else
 				{paint_r, paint_g, paint_b} = 12'h137;
